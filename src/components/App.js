3f0af1c9395c96e6b796973dfeb51ba13442916e
Jesse Brown <Brownjesse8895@yahoo.com>
2021-12-25 17:08:31 -0600

    fetch call w/useEffect

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,23 @@
-// create your App component here
+import {useEffect, useState} from 'react'
+ 
+
+
+
+const App = () => {
+    const [dogsPics, setDogPics] = useState([]);
+    const [isLoaded, setIsLoaded] = useState(false);        
+    useEffect(() => {
+    fetch("https://dog.ceo/api/breeds/image/random")
+      .then((response) => response.json())
+      .then((data) => {
+       setDogPics(data.message)
+       setIsLoaded(true)
+      });
+  }, []);
+  console.log(dogsPics)
+  if (!isLoaded) return <p>Loading...</p>
+    return <img alt="A Random Dog" src={dogsPics}/>
+}
+
+
+export default App
